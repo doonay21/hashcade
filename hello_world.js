a36efdede241b5696d0d04ec63aa488d3f2073e4
Rafal Dunajski <rdunajski@spikon.pl>
2018-02-06 15:35:26 +0100

    Hashing algorithm extended

--- hello_world.js
+++ hello_world.js
@@ -1,13 +1,12 @@
 const hashcade = require('./src/hashcade')
-//hashcade.debug = true
 
 // ------------------------------------------------------------
-// Simplified 512-bit hash example - hashcade is based on function: keccak512("Hello World!")
+// Basic 512-bit hash example - hashcade is based on function: keccak512("Hello World!")
 // ------------------------------------------------------------
 let data = new Buffer("Hello World!", 'utf8')
-let simplified512 = hashcade.simplified512(data)
+let hashBasic512 = hashcade.hashBasic512(data)
 
-console.log('Simplified 512-bit hash from "' + data.toString() + '" is\n' + simplified512 + '\n')
+console.log('Basic 512-bit hash from "' + data.toString() + '" is\n' + hashBasic512 + '\n')
 // ------------------------------------------------------------
 
 // ------------------------------------------------------------
@@ -16,17 +15,23 @@ console.log('Simplified 512-bit hash from "' + data.toString() + '" is\n' + simp
 let pass1 = new Buffer("super_secre_password", 'utf8')
 let pass2 = new Buffer("hiper_secure_password", 'utf8')
 
-let doublePassword512 = hashcade.fullHash(pass1, pass2, 512)
+let doublePassword512 = hashcade.hash(pass1, pass2, 512)
 
 console.log('Double password 512-bit hash from "' + pass1.toString() + '" and "' + pass2 + '" is:\n' + doublePassword512 + '\n')
 // ------------------------------------------------------------
 
 // ------------------------------------------------------------
-// Custom 512-bit hash example
+// Double password 16-bit hash example
 // ------------------------------------------------------------
-let customHashcade = new Buffer("423a35c7", 'hex')
+let hashTiny16 = hashcade.hashTiny16(pass1, pass2)
 
-let custom512 = hashcade.fullHash(data, customHashcade, 512)
+console.log('Double password 16-bit hash from "' + pass1.toString() + '" and "' + pass2 + '" is:\n' + hashTiny16 + '\n')
+// ------------------------------------------------------------
 
-console.log('Custom 512-bit hash from "' + data.toString() + '" with custom hashcade "' + customHashcade.toString('hex') + '" is:\n' + custom512)
+// ------------------------------------------------------------
+// Double password 32-bit hash example
+// ------------------------------------------------------------
+let hashTiny32 = hashcade.hashTiny32(pass1, pass2)
+
+console.log('Double password 32-bit hash from "' + pass1.toString() + '" and "' + pass2 + '" is:\n' + hashTiny32 + '\n')
 // ------------------------------------------------------------
--- src/hashcade.js
+++ src/hashcade.js
@@ -5,7 +5,6 @@ const js_crc = require('js-crc')
 
 let hashcade = (function() {
 	let module = {}
-	module.debug = false
 
 	const finalizers = {
 		224: ['5', '9', 'b', 'f'],
@@ -13,194 +12,172 @@ let hashcade = (function() {
 		384: ['3', '7', 'd'],
 		512: ['1', '2', '6', 'c']
 	}
-	
-	module.fullHash = (data, hashcade, output_size) => {
-		if (!(data instanceof Buffer)) {
-			error("Parameter 'data': is not Buffer!")
+
+	module.hash = (data1, data2, output_size) => {
+		if (!(data1 instanceof Buffer)) {
+			error("Parameter 'data1': is not Buffer!")
 		}
 
-		if (!(hashcade instanceof Buffer)) {
-			error("Parameter 'hashcade': is not Buffer!")
+		if (!(data2 instanceof Buffer)) {
+			error("Parameter 'data2': is not Buffer!")
+		}
+
+		if (data1.length < 1) {
+			error("Parameter 'data1': is empty!")
+		}
+
+		if (data2.length < 1) {
+			error("Parameter 'data2': is empty!")
+		}
+
+		return hash(data1, data2, output_size)
+	}
+
+	module.hashTiny16 = (data1, data2) => {
+		if (!(data1 instanceof Buffer)) {
+			error("Parameter 'data1': is not Buffer!")
+		}
+
+		if (!(data2 instanceof Buffer)) {
+			error("Parameter 'data2': is not Buffer!")
+		}
+
+		if (data1.length < 1) {
+			error("Parameter 'data1': is empty!")
+		}
+
+		if (data2.length < 1) {
+			error("Parameter 'data2': is empty!")
+		}
+
+		let hashcade = new Buffer(js_sha3.keccak512(js_sha3.keccak512(Buffer.concat([data1, data2]))))
+
+		return js_crc.crc16(Buffer.concat([data1, data2, hashcade, new Buffer(hash(data1, data2, 512))]))
+	}
+
+	module.hashTiny32 = (data1, data2) => {
+		if (!(data1 instanceof Buffer)) {
+			error("Parameter 'data1': is not Buffer!")
+		}
+
+		if (!(data2 instanceof Buffer)) {
+			error("Parameter 'data2': is not Buffer!")
+		}
+
+		if (data1.length < 1) {
+			error("Parameter 'data1': is empty!")
+		}
+
+		if (data2.length < 1) {
+			error("Parameter 'data2': is empty!")
+		}
+
+		let hashcade = new Buffer(js_sha3.keccak512(js_sha3.keccak512(data1.toString() + data2.toString())))
+
+		return js_crc.crc32(Buffer.concat([data1, data2, hashcade, new Buffer(hash(data1, data2, 512))]))
+	}
+
+	module.hashBasic224 = data => {
+		return module.hashBasic(data, 224)
+	}
+
+	module.hashBasic256 = data => {
+		return module.hashBasic(data, 256)
+	}
+
+	module.hashBasic384 = data => {
+		return module.hashBasic(data, 384)
+	}
+
+	module.hashBasic512 = data => {
+		return module.hashBasic(data, 512)
+	}
+
+	module.hashBasic = (data, output_size) => {
+		if (!(data instanceof Buffer)) {
+			error("Parameter 'data': is not Buffer!")
 		}
 
 		if (data.length < 1) {
 			error("Parameter 'data': is empty!")
 		}
 
-		if (hashcade.length < 1) {
-			error("Parameter 'hashcade': is empty!")
-		}
-
-		return hash(data, hashcade, output_size)
+		return hash(data, data, output_size)
 	}
 
-	module.secureTiny16 = (data, hashcade) => {
-		if (!(data instanceof Buffer)) {
-			error("Parameter 'data': is not Buffer!")
-		}
+	function hash(data1, data2, output_size) {
+		let hashcade = new Buffer(js_sha3.keccak512(js_sha3.keccak512(data1.toString() + data2.toString())))
 
-		if (!(hashcade instanceof Buffer)) {
-			error("Parameter 'hashcade': is not Buffer!")
-		}
-
-		if (data.length < 1) {
-			error("Parameter 'data': is empty!")
-		}
-
-		if (hashcade.length < 1) {
-			error("Parameter 'hashcade': is empty!")
-		}
-
-		// ToDo crc 16
-
-		return hash(data, hashcade, output_size)
-	}
-
-	module.secureTiny16 = (data, hashcade) => {
-		// ToDo crc 32
-	}
-
-	module.simplified224 = data => {
-		return module.simplified(data, 224)
-	}
-
-	module.simplified256 = data => {
-		return module.simplified(data, 256)
-	}
-
-	module.simplified384 = data => {
-		return module.simplified(data, 384)
-	}
-
-	module.simplified512 = data => {
-		return module.simplified(data, 512)
-	}
-
-	module.simplified = (data, output_size) => {
-		if (!(data instanceof Buffer)) {
-			error("Parameter 'data': is not Buffer!")
-		}
-
-		if (data.length < 1) {
-			error("Parameter 'data': is empty!")
-		}
-
-		return hash(data, new Buffer(js_sha3.keccak512(data.toString()), 'hex'), output_size)
-	}
-
-	function hash(data, hashcade, output_size) {
 		let hashcade_str = hashcade.toString('hex')
 		
-		debug("Input: " + data.toString())
-		debug("Hashcade pre: " + hashcade_str)
-
 		if (finalizers[output_size].indexOf(hashcade_str[hashcade_str.length - 1]) == -1) {
-			let data_hash = js_sha3.keccak512(data.toString('hex'))
 			let sum = 0
 
-			for (let i = 0; i < data_hash.length; i++) {
-				sum += data_hash[i].charCodeAt(0)
+			for (let i = 0; i < hashcade_str.length; i++) {
+				sum += hashcade_str[i].charCodeAt(0)
 			}
 
 			hashcade_str += finalizers[output_size][sum % 4]
-
-			debug("Finalizer generated")
 		}
-
-		debug("Hashcade post: " + hashcade_str)
-		debug("Rounds: " + hashcade_str.length)
 
 		let ret = ""
 
 		for (let i = 0; i < hashcade_str.length; i++) {
-			debug("--------------------")
-			debug("")
-			debug("Round " + (i + 1))
-			debug("")
-
-			let input = Buffer.concat([data, hashcade, new Buffer(ret)])
+			let input = Buffer.concat([data1, data2, hashcade, new Buffer(ret)]).toString()
 
 			switch (hashcade_str[i]) {
 				case '0':
-					debug("Algorithm: shake123-256")
 					ret = js_sha3.shake128(input, 256)
 					break
 				case '1':
-					debug("Algorithm: shake256-512")
 					ret = js_sha3.shake256(input, 512)
 					break
 				case '2':
-					debug("Algorithm: keccak512")
 					ret = js_sha3.keccak512(input)
 					break
 				case '3':
-					debug("Algorithm: keccak384")
 					ret = js_sha3.keccak384(input)
 					break
 				case '4':
-					debug("Algorithm: keccak256")
 					ret = js_sha3.keccak256(input)
 					break
 				case '5':
-					debug("Algorithm: keccak224")
 					ret = js_sha3.keccak224(input)
 					break
 				case '6':
-					debug("Algorithm: sha3_512")
 					ret = js_sha3.sha3_512(input)
 					break
 				case '7':
-					debug("Algorithm: sha3_384")
 					ret = js_sha3.sha3_384(input)
 					break
 				case '8':
-					debug("Algorithm: sha3_256")
 					ret = js_sha3.sha3_256(input)
 					break
 				case '9':
-					debug("Algorithm: sha3_224")
 					ret = js_sha3.sha3_224(input)
 					break
 				case 'a':
-					debug("Algorithm: sha512_256")
 					ret = js_sha512.sha512_256(input)
 					break
 				case 'b':
-					debug("Algorithm: sha512_224")
 					ret = js_sha512.sha512_224(input)
 					break
 				case 'c':
-					debug("Algorithm: sha512")
 					ret = js_sha512.sha512(input)
 					break
 				case 'd':
-					debug("Algorithm: sha384")
 					ret = js_sha512.sha384(input)
 					break
 				case 'e':
-					debug("Algorithm: sha256")
 					ret = js_sha256.sha256(input)
 					break
 				case 'f':
-					debug("Algorithm: sha224")
 					ret = js_sha256.sha224(input)
 					break
 			}
-
-			debug("OUT: " + ret + "\n")
 		}
-
-		debug("--------------------")
-		debug("Generated hash: " + ret)
-		debug("--------------------\n")
 
 		return ret
-	}
-
-	function debug(text) {
-		if (module.debug) {
-			console.log(text)
-		}
 	}
 
 	function error(text) {
